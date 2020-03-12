42abef75e7bbdd5b7b263362997149ef13660445
Diovane1103 <diovane.rossato@ilegra.com>
2020-03-12 15:48:54 -0300

    Adding an idea

--- instagram-clone/api/src/chat/socket.js
+++ instagram-clone/api/src/chat/socket.js
@@ -3,9 +3,14 @@ const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 const Chat = require('./../models/chat')
 
-io.on('connection', async (socket) => {
-    socket.on('join', (options, cb) => {
 
+io.on('connection', (socket) => {
+    // With a click in an existing chat send chatId to socket else create a chat empty sending undefined in chatId
+    socket.on('join', async (chat, cb) => {
+        let chatL = chat
+        if(chatL.data.length > 0)
+            cb(chatL.data)
+        cb(undefined)
     })
 })
 
